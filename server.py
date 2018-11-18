fc5cbc1c47d700b9ac6e61cf1375465c50a2b861
Gabriel Brown <gbrown3@macalester.edu>
2018-11-18 15:12:44 -0600

    Added barebones templates and routes to load new pages

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_url_path='')
 
+
+"""
+Routes to new pages
+"""
 @app.route('/')
 def index():
     return app.send_static_file('login.html')
+
+@app.route('/lobby/<lobby_id>')
+def lobby(lobby_id):
+    return render_template("lobby.html", lobby_id=lobby_id)
+
+@app.route('/game/<lobby_id>')
+def game(lobby_id):
+    return render_template("game.html", lobby_id=lobby_id)
+
+
+"""
+Routes to pass around information
+"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
