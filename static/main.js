241bf14de4937d3f9fb14532cb2b11ddb0028d39
Haimeng Zhang <hzhang1@macalester.edu>
2018-11-23 16:20:32 -0600

    Initial game setup

--- static/main.js
+++ static/main.js
@@ -1,24 +1,19 @@
 
-var game = new Phaser.Game(800, 600, Phaser.WEBGL, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.WEBGL, 'gameDiv');
 
-function preload() {
+var mainState = {
+    preload: function(){
+        
+    },
 
-    game.load.image('sky', 'assets/skies/deepblue.png');
-    game.load.image('dragonTexture', 'assets/creature/dragon.png');
-    game.load.json('dragonMesh', 'assets/creature/dragon.json');
+    create: function() {
 
+    },
+
+    update:function() {
+
+    }
 }
 
-var dragon = null;
-
-function create() {
-
-    game.add.image(0, 0, 'sky');
-
-    dragon = game.add.creature(450, 350, 'dragonTexture', 'dragonMesh');
-
-    dragon.scale.set(25.0);
-
-    dragon.play(true); //  true = loop
-
-}
+game.state.add("mainState", mainState);
+game.state.start("mainState");
