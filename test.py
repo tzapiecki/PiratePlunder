e4906f339fcf7fc71902ce2e16afaf99114894b7
Roland Munsil <roland@munsil.com>
2019-02-19 14:42:45 -0600

    Add first test

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 from server_classes import server_utilities
+import unittest
 
+class TestIsRoomCodeValid(unittest.TestCase):
 
+    def test_simple_valid_codes(self):
+        self.assertTrue(server_utilities.is_room_code_valid("ABC"))
+        self.assertTrue(server_utilities.is_room_code_valid("abc"))
+        self.assertTrue(server_utilities.is_room_code_valid("123"))
+        self.assertTrue(server_utilities.is_room_code_valid("a"))
+        self.assertTrue(server_utilities.is_room_code_valid("abcABC123"))
 
-server_utilities.is_room_code_valid()
+if __name__ == '__main__':
+    unittest.main()
